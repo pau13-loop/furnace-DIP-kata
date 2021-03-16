6753602236c96b446ed7df787b1c7884baac0472
pau <pllinas@cifpfbmoll.eu>
2021-03-16 12:12:56 +0100

    feat: :sparkles: Import main class app

--- furnace/src/main/java/edu/pingpong/furnace/main/FurnaceApp.java
+++ furnace/src/main/java/edu/pingpong/furnace/main/FurnaceApp.java
@@ -1,13 +1,33 @@
 package edu.pingpong.furnace.main;
 
-/**
- * Hello world!
- *
- */
+
+// import org.foobarspam.furnaceDIP.hardware.GasHeater;
+// import org.foobarspam.furnaceDIP.hardware.Regulator;
+// import org.foobarspam.furnaceDIP.hardware.RemoteCommandSensor;
+// import org.foobarspam.furnaceDIP.interfaces.Heater;
+// import org.foobarspam.furnaceDIP.interfaces.Thermometer;
+// import org.foobarspam.furnaceDIP.otherstuff.Jedi;
+// import org.foobarspam.furnaceDIP.types.RoomTemperature;
+
 public class FurnaceApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	final double minTemp = 15.0;
+        final double maxTemp = 21.0;
+        
+        RoomTemperature temperature = new RoomTemperature(15);
+        Heater heater = new GasHeater();
+        Thermometer thermometer = new RemoteCommandSensor();
+        
+        Regulator regulator = new Regulator();
+        
+        System.out.println( "Arrancando..." );
+        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+        
+        Jedi yoda = new Jedi();
+        System.out.println( "\nArrancando a Yoda: " );
+        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
+        yoda.speak();
     }
 }
