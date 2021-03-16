e0b5281e6fb99ae60b58a2ff471b5776717ab547
pau <pllinas@cifpfbmoll.eu>
2021-03-16 11:29:26 +0100

    feat: :sparkles: Setup directory structure for our project

---
+++ furnace/src/main/java/edu/pingpong/furnace/main/FurnaceApp.java
@@ -0,0 +1,13 @@
+package edu.pingpong.furnace.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class FurnaceApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
